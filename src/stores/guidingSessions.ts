ae68e6eda2a0efb1575f4910549e664204fd2e5b
exrelax <exrelax@gmail.com>
2024-07-19 19:31:15 +0200

    feat: Add pinia store persistence

--- src/stores/guidingSessions.ts
+++ src/stores/guidingSessions.ts
@@ -1,6 +1,6 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-import { v4 as uuidv4 } from 'uuid'
+import {ref, computed} from 'vue'
+import {defineStore} from 'pinia'
+import {v4 as uuidv4} from 'uuid'
 import dayjs from 'dayjs'
 
 interface Account {
@@ -22,122 +22,146 @@ interface GuidingSession {
 }
 
 export const useGuidingSessionsStore = defineStore('guidingSessions', () => {
-    const accounts = ref([])
-    const sessions = ref([])
+        const accounts = ref([])
+        const sessions = ref([])
 
-    const createAccount = (handle: string, monitored: boolean = true) => {
-        const account = getAccountByHandle.value(handle)
+        const createAccount = (handle: string, monitored: boolean = true) => {
+            const account = getAccountByHandle.value(handle)
 
-        if (account) {
-            return
-        }
+            if (account) {
+                return
+            }
 
-        accounts.value.push({
-            id: uuidv4(),
-            handle,
-            monitored
-        })
-    }
-
-    const removeAccountById = (id: string) => {
-        const index = accounts.value.findIndex(account => account.id === id)
-
-        if (index > -1) {
-            accounts.value.splice(index, 1)
-        }
-    }
-
-    const removeAccountByHandle = (handle: string) => {
-        const index = accounts.value.findIndex(account => account.handle === handle)
-
-        if (index > -1) {
-            accounts.value.splice(index, 1)
-        }
-    }
-
-    const getAccountById = computed(() => {
-        return (id) => {
-            return accounts.value.find(account => account.id === id)
-        }
-    })
-
-    const getAccountByHandle = computed(() => {
-        return (handle) => {
-            return accounts.value.find(account => account.handle === handle)
-        }
-    })
-
-    const getSessionById = computed(() => {
-        return (id) => {
-            return sessions.value.find(session => session.id === id)
-        }
-    })
-
-    const getSessionByAccountHandle = computed(() => {
-        return (accountHandle: string) => {
-            return sessions.value.filter(session => {
-                return session.roles.some(role => role.account === accountHandle)
+            accounts.value.push({
+                id: uuidv4(),
+                handle,
+                monitored
             })
         }
-    })
 
-    const getGuideSessionByAccountHandle = computed(() => {
-        return (accountHandle: string) => {
-            return sessions.value.filter(session => {
-                return session.roles.some(role => role.accountRole_id === 'guide' && role.account === accountHandle)
-            })
-        }
-    })
+        const removeAccountById = (id: string) => {
+            const index = accounts.value.findIndex(account => account.id === id)
 
-    const getRecruitSessionByAccountHandle = computed(() => {
-        return (accountHandle: string) => {
-            return sessions.value.filter(session => {
-                return session.roles.some(role => role.accountRole_id === 'recruit' && role.account === accountHandle)
-            })
-        }
-    })
-
-    const sessionRolesIsValid = (roles: GuidingSession['roles']) => {
-        return roles.length >= 2 && roles.every(role => role.account && role.accountRole_id)
-    }
-
-    const sessionIsValid = (session: GuidingSession) => {
-        return sessionRolesIsValid(session.roles) &&
-            session.guidingCategory_ids.length > 0 &&
-            session.fromDate != null
-    }
-
-    const addSession = (session: GuidingSession) => {
-        if (!sessionIsValid(session)) {
-            return false
+            if (index > -1) {
+                accounts.value.splice(index, 1)
+            }
         }
 
-        const fromDateDjs = dayjs(session.fromDate)
-        const toDateDjs = session.toDate ? dayjs(session.toDate) : null
-        const completed = session.toDate != null && fromDateDjs.isBefore(toDateDjs)
+        const removeAccountByHandle = (handle: string) => {
+            const index = accounts.value.findIndex(account => account.handle === handle)
 
-        sessions.value.push({
-            ...session,
-            id: uuidv4(),
-            completed
+            if (index > -1) {
+                accounts.value.splice(index, 1)
+            }
+        }
+
+        const getAccountById = computed(() => {
+            return (id) => {
+                return accounts.value.find(account => account.id === id)
+            }
         })
 
-        session.roles.forEach(role => {
-            createAccount(role.account)
+        const getAccountByHandle = computed(() => {
+            return (handle) => {
+                return accounts.value.find(account => account.handle === handle)
+            }
         })
 
-        return true
-    }
+        const getSessionById = computed(() => {
+            return (id) => {
+                return sessions.value.find(session => session.id === id)
+            }
+        })
 
-    return {
-        accounts,
-        sessions,
-        addSession,
-        getAccountById,
-        getAccountByHandle,
-        getSessionById,
-        getSessionByAccountHandle,
-        getGuideSessionByAccountHandle,
-        getRecruitSessionByAccountHandle,
+        const getSessionByAccountHandle = computed(() => {
+            return (accountHandle: string) => {
+                return sessions.value.filter(session => {
+                    return session.roles.some(role => role.account === accountHandle)
+                })
+            }
+        })
+
+        const getGuideSessionByAccountHandle = computed(() => {
+            return (accountHandle: string) => {
+                return sessions.value.filter(session => {
+                    return session.roles.some(role => role.accountRole_id === 'guide' && role.account === accountHandle)
+                })
+            }
+        })
+
+        const getRecruitSessionByAccountHandle = computed(() => {
+            return (accountHandle: string) => {
+                return sessions.value.filter(session => {
+                    return session.roles.some(role => role.accountRole_id === 'recruit' && role.account === accountHandle)
+                })
+            }
+        })
+
+        const sessionRolesIsValid = (roles: GuidingSession['roles']) => {
+            return roles.length >= 2 && roles.every(role => role.account && role.accountRole_id)
+        }
+
+        const sessionIsValid = (session: GuidingSession) => {
+            return sessionRolesIsValid(session.roles) &&
+                session.guidingCategory_ids.length > 0 &&
+                session.fromDate != null
+        }
+
+        const addSession = (session: GuidingSession) => {
+            if (!sessionIsValid(session)) {
+                return false
+            }
+
+            const fromDateDjs = dayjs(session.fromDate)
+            const toDateDjs = session.toDate ? dayjs(session.toDate) : null
+            const completed = session.toDate != null && fromDateDjs.isBefore(toDateDjs)
+
+            sessions.value.push({
+                ...session,
+                id: uuidv4(),
+                completed
+            })
+
+            session.roles.forEach(role => {
+                createAccount(role.account)
+            })
+
+            return true
+        }
+
+        const restoreSessionDates = () => {
+            sessions.value = sessions.value.map(session => {
+                const fromDate = new Date(session.fromDate)
+                const toDate = session.toDate ? new Date(session.toDate) : null
+
+                return {
+                    ...session,
+                    fromDate,
+                    toDate
+                }
+            })
+        }
+
+        return {
+            accounts,
+            sessions,
+            addSession,
+            getAccountById,
+            getAccountByHandle,
+            getSessionById,
+            getSessionByAccountHandle,
+            getGuideSessionByAccountHandle,
+            getRecruitSessionByAccountHandle,
+            restoreSessionDates,
+        }
+    },
+    {
+        persist: {
+            storage: localStorage,
+            paths: [ 'accounts', 'sessions' ],
+            afterRestore: (ctx) => {
+                ctx.store.restoreSessionDates()
+            }
+        }
     }
-})
+)
